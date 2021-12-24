8a93f0b9896d6052d0662f3e4b12e9d132ade367
Jonas Carpay <jonascarpay@gmail.com>
2021-12-24 17:09:33 +0900

    Expand import list

--- src/Nonlinear.hs
+++ src/Nonlinear.hs
@@ -1,6 +1,10 @@
 module Nonlinear (module X) where
 
+import Nonlinear.Matrix as X
+import Nonlinear.Projection as X
+import Nonlinear.Quaternion as X
 import Nonlinear.V1 as X
 import Nonlinear.V2 as X
 import Nonlinear.V3 as X
 import Nonlinear.V4 as X
+import Nonlinear.Vector as X
