38055cfc944dc997b2e23f53633f2de1bc7c7966
Jonas Carpay <jonascarpay@gmail.com>
2021-12-28 16:47:20 +0900

    More sanity check property tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
-import Data.Functor.Identity (Identity, runIdentity)
+import Data.Coerce (coerce)
+import Data.Functor.Identity (Identity (Identity), runIdentity)
+import Data.Ratio
 import Nonlinear
 import Test.Hspec
 import Test.Hspec.QuickCheck
@@ -37,24 +39,49 @@ deriving instance
   Show (VSome f a)
 
 instance (Arbitrary a, forall a. Arbitrary a => Arbitrary (f a)) => Arbitrary (VSome f a) where
-  arbitrary =
-    oneof
-      [ VS1 <$> arbitrary,
-        VS2 <$> arbitrary,
-        VS3 <$> arbitrary,
-        VS4 <$> arbitrary
-      ]
+  arbitrary = oneof [VS1 <$> arbitrary, VS2 <$> arbitrary, VS3 <$> arbitrary, VS4 <$> arbitrary]
 
-withVSome :: Eq a => (forall v. (Eq (v a), Applicative v, Traversable v) => f (v a) -> r) -> (VSome f a -> r)
+withVSome ::
+  ( forall v.
+    (forall a. Eq a => Eq (v a), Monad v, Traversable v, R1 v) =>
+    f (v a) ->
+    r
+  ) ->
+  (VSome f a -> r)
 withVSome f (VS1 v) = f v
 withVSome f (VS2 v) = f v
 withVSome f (VS3 v) = f v
 withVSome f (VS4 v) = f v
 
 prop_transpose_involutive :: VSome (VSome Identity) Int -> Bool
-prop_transpose_involutive = withVSome (withVSome $ (\mat -> sequenceA (sequenceA mat) == mat) . runIdentity)
+prop_transpose_involutive = withVSome $ withVSome $ \(Identity mat) -> sequenceA (sequenceA mat) == mat
+
+prop_column_id_is_id :: VSome (VSome Identity) Int -> Bool
+prop_column_id_is_id = withVSome $ withVSome $ \(Identity mat) -> over (column id) id mat == mat
+
+prop_column_x_id_is_id :: VSome (VSome Identity) Int -> Bool
+prop_column_x_id_is_id = withVSome $ withVSome $ \(Identity mat) -> over (column _x) id mat == mat
+
+over :: forall s t a b. ((a -> Identity b) -> (s -> Identity t)) -> (a -> b) -> (s -> t)
+over = coerce
+
+prop_diagonal_scaled_identity :: VSome Identity Int -> Bool
+prop_diagonal_scaled_identity = withVSome $ \(Identity vec) -> diagonal (scaled vec) == vec
+
+prop_project_self_identity :: VSome Identity (Ratio Int) -> Bool
+prop_project_self_identity = withVSome $ \(Identity vec) -> project vec vec == vec
+
+prop_normalize_idempotent :: VSome Identity Double -> Property
+prop_normalize_idempotent = withVSome $ \(Identity vec) ->
+  not (all (== 0) vec)
+    ==> fmap round (normalize (normalize vec)) == fmap round (normalize vec)
 
 main :: IO ()
 main = hspec $ do
   describe "distribute" $ do
-    prop "transpose involutive" prop_transpose_involutive
+    prop "transpose is involutive" prop_transpose_involutive
+    prop "(diagonal . scaled) is identity" prop_transpose_involutive
+    prop "(project x x) is identity" prop_transpose_involutive
+    prop "normalize is idempotent" prop_normalize_idempotent
+    prop "over (column id) id is identity " prop_column_id_is_id
+    prop "over (column _x) id is identity " prop_column_x_id_is_id
